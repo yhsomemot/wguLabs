2ed7609c0be2415584e721f9c56e0cfe4079b888
yhsomemot <142277607+yhsomemot@users.noreply.github.com>
2024-12-06 15:27:33 -0600

    Create 2.21.sql

---
+++ answers/2.21.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Horse (
+	ID              SMALLINT UNSIGNED AUTO_INCREMENT,
+	RegisteredName  VARCHAR(15),
+	PRIMARY KEY (ID)
+);
+
+CREATE TABLE Student (
+	ID             SMALLINT UNSIGNED AUTO_INCREMENT,
+	FirstName      VARCHAR(20),
+	LastName 		VARCHAR(30),
+	PRIMARY KEY (ID)
+);
+
+CREATE TABLE LessonSchedule (
+   HorseID        SMALLINT UNSIGNED NOT NULL,
+   StudentID      SMALLINT UNSIGNED,
+   LessonDateTime DATETIME NOT NULL,
+   PRIMARY KEY (HorseID, LessonDateTime),
+   FOREIGN KEY (HorseID) REFERENCES Horse(ID)
+      ON DELETE CASCADE,
+   FOREIGN KEY (StudentID) REFERENCES Student(ID)
+      ON DELETE SET NULL
+);
