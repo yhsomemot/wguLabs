10d12efee6b58e658bd12f17c1576d51903b2baa
yhsomemot <142277607+yhsomemot@users.noreply.github.com>
2024-12-06 15:15:52 -0600

    Update and rename 2.16.txt to 2.16.sql

---
+++ answers/2.16.sql
@@ -0,0 +1,6 @@
+INSERT INTO Horse (RegisteredName, Breed, Height, BirthDate)
+VALUES
+  ('Babe', 'Quarter Horse', 15.3, '2015-02-10'),
+  ('Independence', 'Holsteiner', 16.0, '2017-03-13'),
+  ('Ellie', 'Saddlebred', 15.0, '2016-12-22'),
+  (NULL, 'Egyptian Arab', 14.9, '2019-10-12');
